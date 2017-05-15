94fe376e6de312ba553492786c87e3e39e795bc4
MartinBulla <MartinBulla>
2017-05-15 15:21:29 +0200

    first raw analyses and figures

--- Analyses.R
+++ Analyses.R
@@ -44,7 +44,10 @@
 	#d$rad_st= 2*d$hour*pi/24
 	d$rad_st= 2*d$dast*pi/14.75
 	
+	
 	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year), family = 'binomial', d) 
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year), family = 'binomial', d) 
+	f
 	plot(allEffects(m))
 	summary(glht(m))
 	
@@ -108,8 +111,8 @@
 						# legend
 							#legend('topright', symbols(
 							mtext(expression(italic('N')*' nests:'),side = 4,line=0.1, padj=-7,cex=0.5,las=1,col='grey30', xpd=TRUE) # for printing into device use padj=-7.5
-							symbols(c(17,17,17),c(26,26-4,26-4*2),circles=sqrt(c(1,30,75)/pi),inches=0.14/1.75,bg=col_pb, fg=col_p,add=TRUE, xpd=TRUE) #bg=alpha(col_p,0.1)
-														
+							symbols(c(17,17,17)+1,c(26,26-4,26-4*2),circles=sqrt(c(1,30,75)/pi),inches=0.14/1.75,bg=col_pb, fg=col_p,add=TRUE, xpd=TRUE) #bg=alpha(col_p,0.1)
+							text(c(17,17,17)+4,c(26,26-4,26-4*2),labels=c(1,30,75), xpd=NA, cex=0.5,col='grey30')													
 							#text(c(7.1),c(2.05),labels=c('Median & IQR'), xpd=TRUE, cex=0.5,col='grey30', srt=90, xpd=FALSE) 
 							
 							#mtext('Predictions &\n95%CrI',side = 4,line=-0.3,padj=2, cex=0.5,las=1,col='red', xpd=TRUE) 
@@ -120,12 +123,12 @@
 	}			
 	{# plot 0:100
 		if(PNG == TRUE) {
-			png(paste(outdir,"Figure_flood.png", sep=""), width=1.85,height=1.5,units="in",res=600) 
+			png(paste(outdir,"Figure_flood_means_pre_year.png", sep=""), width=1.85,height=1.5,units="in",res=600) 
 					}else{
 					dev.new(width=1.85,height=1.5)
 					}	
 				
-				par(mar=c(0.25,0.5,0,1.2),oma = c(1.5, 1.5, 0.2, 1.5),ps=12, mgp=c(1.2,0.35,0), las=1, cex=1, col.axis="grey30",font.main = 1, col.lab="grey30", col.main="grey30", fg="grey70", cex.lab=0.6,cex.main=0.7, cex.axis=0.5, tcl=-0.1,bty="n",xpd=TRUE) #
+				par(mar=c(0.25,0.5,0.2,1.2),oma = c(1.5, 1.5, 0.2, 1.5),ps=12, mgp=c(1.2,0.35,0), las=1, cex=1, col.axis="grey30",font.main = 1, col.lab="grey30", col.main="grey30", fg="grey70", cex.lab=0.6,cex.main=0.7, cex.axis=0.5, tcl=-0.1,bty="n",xpd=TRUE) #
 						
 				plot(pp$pred~pp$dast, 
 						xlim=c(0,15), ylim=c(0,100),
@@ -137,26 +140,22 @@
 						mtext("Days from spring tide",side=1,line=0.4, cex=0.5, las=1, col='grey30')
 						mtext("Flooded nests [%]",side=2,line=1, cex=0.6, las=3, col='grey30')
 						
-						#symbols( jitter(x$dast),jitter(x$mean_), circles=sqrt(x$n/pi),inches=0.14/1.75,bg=adjustcolor(x$col_, alpha.f = 0.5), fg=col_p,add=TRUE) 
-						symbols( jitter(x2$dast),jitter(x2$mean_), circles=sqrt(x2$n/pi),inches=0.14/1.75,bg=adjustcolor(col_f, alpha.f = 0.5), fg=col_p,add=TRUE) 
+						symbols( jitter(x$dast),jitter(x$mean_), circles=sqrt(x$n/pi),inches=0.14/1.75,bg=adjustcolor(x$col_, alpha.f = 0.5), fg=col_p,add=TRUE) 
+						#symbols( jitter(x2$dast),jitter(x2$mean_), circles=sqrt(x2$n/pi),inches=0.14/1.75,bg=adjustcolor(col_f, alpha.f = 0.5), fg=col_p,add=TRUE) 
 						
 						polygon(c(pp$dast, rev(pp$dast)), c(pp$lwr, 
 								rev(pp$upr)), border=NA, col=adjustcolor(col_m ,alpha.f = 0.2)) #0,0,0 black 0.5 is transparents RED
 							lines(pp$dast, pp$pred, col=col_m,lwd=1)
 							
+						if(PNG == TRUE) {dev.off()}
 						
-						# legend
-							mtext(expression(italic('N')*' observations:'),side = 4,line=-0.3, padj=-7,cex=0.5,las=1,col='grey30', xpd=TRUE) # for printing into device use padj=-7.5
-							symbols(c(8.5,8.5,8.5),c(3.3,2.8,2.3)-0.5,circles=sqrt(c(1,10,20)/pi),inches=0.14/1.75,bg=col_pb, fg=col_p,add=TRUE, xpd=TRUE) #bg=alpha(col_p,0.1)
-							text(c(8.5,8.5,8.5)+1,c(3.3,2.8,2.3)-0.5,labels=c(1,10,20), xpd=TRUE, cex=0.5,col='grey30') 
-							
-							text(c(7.1),c(2.05),labels=c('Median & IQR'), xpd=TRUE, cex=0.5,col='grey30', srt=90, xpd=FALSE) 
-							
-							mtext('Predictions &\n95%CrI',side = 4,line=-0.3,padj=2, cex=0.5,las=1,col='red', xpd=TRUE) 
-									# use if plotting within RData
-									#mtext('Weighted\nmedian',side = 4,line=3, cex=0.5,padj=-3.25,adj=0.5, las=1,col='grey30',xpd=TRUE)
-									#points(12.5, 85, pch=19, col='black',xpd=NA)
-		if(PNG == TRUE) {dev.off()}
+						
 	}			
 					
+}
+{# 
+
+}
+{# moon light
+d$rad_nm= 2*d$danm*pi/29.5
 }	
