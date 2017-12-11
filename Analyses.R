5d6dc10c251411f60358efe4fff705018fa37e12
MartinBulla <MartinBulla>
2017-12-11 10:38:28 +0100

    added success anal

--- Analyses.R
+++ Analyses.R
@@ -17,6 +17,7 @@
 
 {# FLOODING
 	d = nn[!is.na(nn$fate),]
+	summary(factor(d$fate))
 	d$fate[d$fate=='tide'] = 'flood'
 	d$flooded = ifelse(d$fate=='flood',1,0)
 	d$flooded_ = ifelse(d$fate=='flood','yes','no')
@@ -546,6 +547,250 @@
 		
 		}
 
+			
+{# HATCHING
+	d = nn[!is.na(nn$fate),]
+	summary(factor(d$fate))
+	d$fate[d$fate=='tide'] = 'flood'
+	d$success = ifelse(d$fate=='hatch','yes','no')
+	d$success_ = ifelse(d$fate=='hatch',1,0)
+	d$year_ = as.factor(d$year)
+
+	#d$rad_st= 2*d$hour*pi/24
+	d$rad_st= 2*d$days_after_st*pi/14.765 #or half a synodic month of 29.53 days 
+	d$year_tc=factor(paste(d$year,d$st_cycle))
+	
+	ggplot(d,aes(x = days_after_st, fill = success)) + geom_histogram()+facet_grid(year ~ .)
+	ggplot(d,aes(x = days_after_st, fill = success)) + geom_density(alpha = 0.5)+facet_grid(year ~ .)
+	ggplot(d[d$fate=='hatch',],aes(x = days_after_st, col=factor(st_cycle))) + geom_density()
+	
+	# simple
+	#m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|st_cycle)+(1|pair), family = 'binomial', d) 
+	m = glmer(success_ ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|st_cycle)+(1|female), family = 'binomial', d) 
+	m = glmer(success_ ~ sin(rad_st) + cos(rad_st) +(sin(rad_st) + cos(rad_st)|year_)+(1|st_cycle)+(1|female), family = 'binomial', d)
+				# optimize
+					# fails
+						ss <- getME(m,c("theta","fixef"))
+						m2 <- update(m,start=ss,control=glmerControl(optCtrl=list(maxfun=2e4)))
+					# optimizers
+						# bobyqa - runs
+							m3 <- update(m,start=ss,control=glmerControl(optimizer="bobyqa",
+                            optCtrl=list(maxfun=2e5)))
+								plot(allEffects(m3))
+								summary(glht(m3))
+								summary(m3)
+						# nloptwrap
+							m4 <- update(m,start=ss,control=glmerControl(optimizer="nloptwrap",
+                            optCtrl=list(maxfun=2e5)))
+								plot(allEffects(m3))
+								summary(glht(m3))
+								summary(m3)
+						# try all optimizers
+							aa <- allFit(m)
+							# bobyqa, nlminbw, optimx.L-BFGS-B ok, others failed
+							is.OK <- sapply(aa,is,"merMod")  
+							aa.OK <- aa[is.OK]
+							lapply(aa.OK,function(x) x@optinfo$conv$lme4$messages)
+							# how bad was the poor convergence in some optimizers
+							(lliks <- sort(sapply(aa.OK,logLik))) # very similar logLik
+							aa.fixef <- t(sapply(aa.OK,fixef))
+							aa.fixef.m <- melt(aa.fixef)
+							models <- levels(aa.fixef.m$Var1)
+							ylabs <- substr(models,1,3)
+							aa.fixef.m <- transform(aa.fixef.m,Var1=factor(Var1,levels=names(lliks)))
+							(gplot1 <- ggplot(aa.fixef.m,aes(x=value,y=Var1,colour=Var1))+geom_point()+
+								 facet_wrap(~Var2,scale="free")+
+									 scale_colour_brewer(palette="Dark2")+
+										 scale_y_discrete(breaks=models,
+														  labels=ylabs)+
+															  labs(x="",y=""))
+							
+						#Coefficients of variation of fixed-effect parameter estimates:
+						summary(unlist(daply(aa.fixef.m,"Var2",summarise,sd(value)/abs(mean(value)))))
+						
+						# variances of the random parameters
+						aa.stddev <- t(sapply(aa.OK,function(x) sqrt(unlist(VarCorr(x)))))
+						print(aa.stddev,digits=3) # NA issues
+						
+						aa.stddev.m <- melt(aa.stddev) 
+						aa.stddev.m <- transform(aa.stddev.m,Var1=factor(Var1,levels=names(lliks)))
+						gplot1 %+% aa.stddev.m # does not work, likely because of the NAs
+						
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|st_cycle)+(1|pair), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|pair), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+	
+	# year
+	d$year_= as.factor(d$year)
+	m = glmer(flooded ~ sin(rad_st)*year_ + cos(rad_st)*year_ +(1|year_)+(1|st_cycle), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+	
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) + (sin(rad_st) + cos(rad_st)|year), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) + (sin(rad_st) + cos(rad_st)|st_cycle), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+	m = glmer(flooded ~ sin(rad_st) + cos(rad_st) + scale(st_cycle) + (1|year)+(1|pair), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+		
+	plot(allEffects(m))
+	summary(glht(m))
+	summary(m)
+	
+	{# predictions
+			m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year), family = 'binomial', d) 
+						nsim <- 5000
+						bsim <- sim(m, n.sim=nsim)  
+				
+				# coefficients
+					v = apply(bsim@fixef, 2, quantile, prob=c(0.5))
+					ci = apply(bsim@fixef, 2, quantile, prob=c(0.025, 0.975))
+				# values to predict for		
+					newD=data.frame(days_after_st = seq(0,15, length.out=300))
+					newD$rad_st =2*newD$days_after_st*pi/14.75
+						
+				# exactly the model which was used has to be specified here 
+					X <- model.matrix(~ sin(rad_st)+cos(rad_st),data=newD)	
+								
+				# calculate predicted values and creditability intervals
+					newD$pred <-plogis(X%*%v) *100
+							predmatrix <- matrix(nrow=nrow(newD), ncol=nsim)
+							for(i in 1:nsim) predmatrix[,i] <- plogis(X%*%bsim@fixef[i,])
+							newD$lwr <- apply(predmatrix, 1, quantile, prob=0.025)*100
+							newD$upr <- apply(predmatrix, 1, quantile, prob=0.975)*100
+					pp=newD	
+	}
+	{# raw data
+		x = ddply(d,.(year, days_after_st), summarise, mean_ = mean(flooded)*100, sd_ = sd(flooded)*100, n = length(year))
+			y = data.frame(year = unique(d$year), col_ =1:length(unique(d$year)), stringsAsFactors = FALSE)
+			x$col_ = y$col_[match(x$year, y$year)]
+			
+		x2 = ddply(d,.(days_after_st), summarise, mean_ = mean(flooded)*100, sd_ = sd(flooded)*100, n = length(year))	
+	}
+	{# plot 0:40
+		if(PNG == TRUE) {
+			png(paste(outdir,"Figure_flood_update.png", sep=""), width=1.85,height=1.5,units="in",res=600) 
+					}else{
+					dev.new(width=1.85,height=1.5)
+					}	
+				
+				par(mar=c(0.25,0.5,0,1.2),oma = c(1.5, 1.5, 0.2, 1.5),ps=12, mgp=c(1.2,0.35,0), las=1, cex=1, col.axis="grey30",font.main = 1, col.lab="grey30", col.main="grey30", fg="grey70", cex.lab=0.6,cex.main=0.7, cex.axis=0.5, tcl=-0.1,bty="n",xpd=TRUE) #
+						
+				plot(pp$pred~pp$days_after_st, 
+						xlim=c(0,15), ylim=c(0,30),
+						xaxt='n',
+						xaxs = 'i', yaxs = 'i',
+						ylab = "Flodded nests [%]",xlab = NULL,type='n')
+						
+						axis(1, at=seq(0,15, by = 3), label=TRUE, mgp=c(0,-0.20,0))
+						mtext("Days from spring tide",side=1,line=0.4, cex=0.5, las=1, col='grey30')
+						mtext("Flooded nests [%]",side=2,line=1, cex=0.6, las=3, col='grey30')
+						
+						
+						polygon(c(pp$days_after_st, rev(pp$days_after_st)), c(pp$lwr, 
+								rev(pp$upr)), border=NA, col=col_lb) #0,0,0 black 0.5 is transparents RED
+							lines(pp$days_after_st, pp$pred, col=col_l,lwd=1)
+						#symbols( jitter(x$days_after_st),jitter(x$mean_), circles=sqrt(x$n/pi),inches=0.14/1.75,bg=adjustcolor(x$col_, alpha.f = 0.5), fg=col_p,add=TRUE) 
+						symbols( jitter(x2$days_after_st),jitter(x2$mean_), circles=sqrt(x2$n/pi),inches=0.14/1.75,bg=col_pb, fg=col_p,add=TRUE) 
+
+						
+						# legend
+							#legend('topright', symbols(
+							mtext(expression(italic('N')*' nests:'),side = 4,line=0.1, padj=-7,cex=0.5,las=1,col='grey30', xpd=TRUE) # for printing into device use padj=-7.5
+							symbols(c(17,17,17)+1,c(26,26-4,26-4*2),circles=sqrt(c(1,30,75)/pi),inches=0.14/1.75,bg=col_pb, fg=col_p,add=TRUE, xpd=TRUE) #bg=alpha(col_p,0.1)
+							text(c(17,17,17)+4,c(26,26-4,26-4*2),labels=c(1,30,75), xpd=NA, cex=0.5,col='grey30')													
+							#text(c(7.1),c(2.05),labels=c('Median & IQR'), xpd=TRUE, cex=0.5,col='grey30', srt=90, xpd=FALSE) 
+							
+							#mtext('Predictions &\n95%CrI',side = 4,line=-0.3,padj=2, cex=0.5,las=1,col='red', xpd=TRUE) 
+									# use if plotting within RData
+									#mtext('Weighted\nmedian',side = 4,line=3, cex=0.5,padj=-3.25,adj=0.5, las=1,col='grey30',xpd=TRUE)
+									#points(12.5, 85, pch=19, col='black',xpd=NA)
+		if(PNG == TRUE) {dev.off()}
+	}			
+	{# plot 0:100
+		if(PNG == TRUE) {
+			png(paste(outdir,"Figure_flood_means_pre_year_update.png", sep=""), width=1.85,height=1.5,units="in",res=600) 
+					}else{
+					dev.new(width=1.85,height=1.5)
+					}	
+				
+				par(mar=c(0.25,0.5,0.2,1.2),oma = c(1.5, 1.5, 0.2, 1.5),ps=12, mgp=c(1.2,0.35,0), las=1, cex=1, col.axis="grey30",font.main = 1, col.lab="grey30", col.main="grey30", fg="grey70", cex.lab=0.6,cex.main=0.7, cex.axis=0.5, tcl=-0.1,bty="n",xpd=TRUE) #
+						
+				plot(pp$pred~pp$days_after_st, 
+						xlim=c(0,15), ylim=c(0,100),
+						xaxt='n',
+						xaxs = 'i', yaxs = 'i',
+						ylab = "Flodded nests [%]",xlab = NULL,type='n')
+						
+						axis(1, at=seq(0,15, by = 3), label=TRUE, mgp=c(0,-0.20,0))
+						mtext("Days from spring tide",side=1,line=0.4, cex=0.5, las=1, col='grey30')
+						mtext("Flooded nests [%]",side=2,line=1, cex=0.6, las=3, col='grey30')
+						
+						symbols( jitter(x$days_after_st),jitter(x$mean_), circles=sqrt(x$n/pi),inches=0.14/1.75,bg=adjustcolor(x$col_, alpha.f = 0.5), fg=col_p,add=TRUE) 
+						#symbols( jitter(x2$days_after_st),jitter(x2$mean_), circles=sqrt(x2$n/pi),inches=0.14/1.75,bg=adjustcolor(col_f, alpha.f = 0.5), fg=col_p,add=TRUE) 
+						
+						polygon(c(pp$days_after_st, rev(pp$days_after_st)), c(pp$lwr, 
+								rev(pp$upr)), border=NA, col=adjustcolor(col_m ,alpha.f = 0.2)) #0,0,0 black 0.5 is transparents RED
+							lines(pp$days_after_st, pp$pred, col=col_m,lwd=1)
+							
+						if(PNG == TRUE) {dev.off()}
+						
+						
+	}			
+		{# model assumptions
+			m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|st_cycle)+(1|pair), family = 'binomial', d) 
+			#m = glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year)+(1|pair), family = 'binomial', d[d$fate%in%c('flood','hatch'),]) 
+		
+			#png(paste(out_,"model_ass/Supplementary_Table_2.png", sep=""), width=6,height=9,units="in",res=600)
+			dev.new(width=6,height=9)
+			par(mfrow=c(5,3),oma = c(0, 0, 1.5, 0) )
+									 								  
+			scatter.smooth(fitted(m),resid(m),col='red');abline(h=0, lty=2)
+			scatter.smooth(fitted(m),sqrt(abs(resid(m))), col='red')
+			qqnorm(resid(m), main=list("Normal Q-Q Plot: residuals", cex=0.8),col='red') 
+			qqline(resid(m))
+									  
+				qqnorm(unlist(ranef(m)$year [1]), main = " intercept",col='red')
+				qqline(unlist(ranef(m)$year [1]))
+				
+				qqnorm(unlist(ranef(m)$pair [1]), main = " intercept",col='red')
+				qqline(unlist(ranef(m)$pair [1]))
+									  
+				#qqnorm(unlist(ranef(m)$nest_ID[2]), main = " slope",col='red')
+				#qqline(unlist(ranef(m)$nest_ID[2]))
+			
+			plot(fitted(m), jitter(d$flooded, amount=0.05), xlab="Fitted values", ylab="Flooded", las=1, cex.lab=1.2, cex=0.8)
+				abline(0,1, lty=3)
+				t.breaks <- cut(fitted(m), quantile(fitted(m)))
+				means <- tapply(d$flooded, t.breaks, mean)
+				semean <- function(x) sd(x)/sqrt(length(x))
+				means.se <- tapply(d$flooded, t.breaks, semean)
+				points(quantile(fitted(m),c(0.125,0.375,0.625,0.875)), means, pch=16, col="orange")
+				segments(quantile(fitted(m),c(0.125,0.375,0.625,0.875)), means-2*means.se, quantile(fitted(m),c(0.125,0.375,0.625,0.875)), means+2*means.se,lwd=2, col="orange")
+										
+				#scatter.smooth(resid(m)~x2$date[x2$sum>720]);abline(h=0, lty=2, col='red')
+				scatter.smooth(resid(m)~sin(d$rad_st));abline(h=0, lty=2, col='red')
+				scatter.smooth(resid(m)~cos(d$rad_st));abline(h=0, lty=2, col='red')
+				scatter.smooth(resid(m)~d$rad_st);abline(h=0, lty=2, col='red')
+				#boxplot(resid(m)~bb_$type);abline(h=0, lty=2, col='red')
+									  
+				mtext("glmer(flooded ~ sin(rad_st) + cos(rad_st) +(1|year), family = 'binomial', d)", side = 3, line = 0.5, cex=0.8,outer = TRUE)
+									   
+				acf(resid(m), type="p", main=list("Temporal autocorrelation:\npartial series residual",cex=0.8))
+									  # spatial autocorrelations - nest location
+				spdata=data.frame(resid=resid(m), x=d$lon, y=d$lat)
+				spdata=spdata[-which(is.na(spdata$y)),]
+						spdata$col=ifelse(spdata$resid<0,rgb(83,95,124,100, maxColorValue = 255),ifelse(spdata$resid>0,rgb(253,184,19,100, maxColorValue = 255), 'red'))
+											#cex_=c(1,2,3,3.5,4)
+						cex_=c(1,1.5,2,2.5,3)
+						spdata$cex=as.character(cut(abs(spdata$resid), 5, labels=cex_))
+						
+						plot(spdata$x, spdata$y,col=spdata$col, cex=as.numeric(spdata$cex), pch= 16, main=list('Spatial distribution of residuals', cex=0.8))
+						legend("topleft", pch=16, legend=c('>0','<0'), ,col=c(rgb(83,95,124,100, maxColorValue = 255),rgb(253,184,19,100, maxColorValue = 255)), cex=0.8)
+						
+						plot(spdata$x[spdata$x<23.930], spdata$y[spdata$x<23.930],col=spdata$col[spdata$x<23.930], cex=as.numeric(spdata$cex[spdata$x<23.930]), pch= 16, main=list('Spatial distribution of residuals', cex=0.8))
+						
+						plot(spdata$x[spdata$resid<0], spdata$y[spdata$resid<0],col=spdata$col[spdata$resid<0], cex=as.numeric(spdata$cex[spdata$resid<0]), pch= 16, main=list('Spatial distribution of residuals', cex=0.8))
+						plot(spdata$x[spdata$resid>=0], spdata$y[spdata$resid>=0],col=spdata$col[spdata$resid>=0], cex=as.numeric(spdata$cex[spdata$resid>=0]), pch= 16, main=list('Spatial distribution of residuals', cex=0.8))
+								
+						dev.off()
+		
+		}
+}
 
 
 {# random sampling of days within the year
